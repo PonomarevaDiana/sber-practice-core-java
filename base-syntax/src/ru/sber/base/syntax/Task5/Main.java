9fdd2353ecc272afd981768d8168f1e6e7a38a6f
Диана <diana7ponomareva@yandex.ru>
2024-02-19 22:47:02 +0300

    Task 5

---
+++ base-syntax/src/ru/sber/base/syntax/Task5/Main.java
@@ -0,0 +1,33 @@
+package ru.sber.base.syntax.Task5;
+import java.util.Scanner;
+public class Main {
+    public static class Task5
+    {
+        public static void main(String[] args){
+            int a,b,c;
+            Scanner sc = new Scanner(System.in);
+            System.out.println(" Введите n");
+            System.out.print(" n = ");
+            int n = sc.nextInt();
+            if (n>=2 && n<=100) {
+                a = 1;
+                b = 1;
+                System.out.print(a);
+                System.out.print(" ");
+                System.out.print(b);
+                System.out.print(" ");
+                for (int i = 0; i<n-2; i++){
+                    c = b;
+                    b = a + b;
+                    System.out.print(b);
+                    System.out.print(" ");
+                    a = c;
+                }
+            }
+            else{
+                System.out.print(" Допустимые значения 2<=n<=100");
+            }
+        }
+    }
+
+}
