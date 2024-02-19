47967f6ca3463400f3da3999c17a4d3f105c4422
Диана <diana7ponomareva@yandex.ru>
2024-02-19 21:58:39 +0300

    Task1

---
+++ base-syntax/src/ru/sber/base/syntax/Task1/Main.java
@@ -0,0 +1,11 @@
+package ru.sber.base.syntax.Task1;
+import java.util.Scanner;
+public class Main {
+    public static class Task1 {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String s = sc.nextLine();
+            System.out.println("Hello, " + s);
+        }
+    }
+}
