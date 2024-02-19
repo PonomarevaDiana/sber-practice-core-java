40c80b21e16842fe1f31476321bbd2548b0fd58f
Диана <diana7ponomareva@yandex.ru>
2024-02-20 01:51:29 +0300

    Task 10

---
+++ base-syntax/src/ru/sber/base/syntax/Task10/Main.java
@@ -0,0 +1,9 @@
+package ru.sber.base.syntax.Task10;
+
+public class Main {
+    public static class Task10{
+        public static void main(String[] args) {
+            javax.swing.JOptionPane.showMessageDialog(null,"Привет,Диана");
+        }
+    }
+}
