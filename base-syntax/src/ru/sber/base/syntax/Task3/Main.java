eb7637dc361a6771c6a51ee165a0deece33c5259
Диана <diana7ponomareva@yandex.ru>
2024-02-20 10:53:51 +0300

    Task 3

--- base-syntax/src/ru/sber/base/syntax/Task3/Main.java
+++ base-syntax/src/ru/sber/base/syntax/Task3/Main.java
@@ -1,7 +1,7 @@
 package ru.sber.base.syntax.Task3;
 public class Main {
-    public static class Task3
-    { public static void main(String[] args){
+    public static class Task3 {
+        public static void main(String[] args){
             for (int i=1; i<=6; i++){
                 for (int j=1; j<=19; j++){
                     if (i%2!=0){
@@ -12,7 +12,7 @@ public class Main {
                             System.out.print(" ");
                         }
                     }
-                    else{
+                    else {
                         if (j%2!=0){
                             System.out.print(" ");
                         }
