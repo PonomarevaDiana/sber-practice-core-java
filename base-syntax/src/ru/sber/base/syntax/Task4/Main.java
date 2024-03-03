a3775b8fade99ec0ad70aced082e582341daf5d9
Диана <diana7ponomareva@yandex.ru>
2024-03-03 20:30:28 +0300

    Task 4

--- base-syntax/src/ru/sber/base/syntax/Task4/Main.java
+++ base-syntax/src/ru/sber/base/syntax/Task4/Main.java
@@ -1,22 +1,18 @@
 package ru.sber.base.syntax.Task4;
+
 import java.util.Scanner;
 import java.lang.Math;
+
 public class Main {
-    public static class Task4
-    {
-        public static void main(String[] args){
+    public static class Task4 {
+        public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             int n = sc.nextInt();
             int i = 2;
-            while(i<=Math.round(Math.sqrt(n)) && n%i!=0){
+            while (i <= Math.sqrt(n) && n % i != 0) {
                 i++;
             }
-            if (i>Math.round(Math.sqrt(n))){
-                System.out.print("Да");
-            }
-            else {
-                System.out.println("Нет");
-            }
+            System.out.println((i > Math.sqrt(n) && n > 1) ? "Да" : "Нет");
         }
     }
 }
