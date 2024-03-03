5504a9dfedb418371db5577e7ebf6e84de21c5a6
Диана <diana7ponomareva@yandex.ru>
2024-03-03 20:30:49 +0300

    Task 7

--- base-syntax/src/ru/sber/base/syntax/Task7/Main.java
+++ base-syntax/src/ru/sber/base/syntax/Task7/Main.java
@@ -1,10 +1,11 @@
 package ru.sber.base.syntax.Task7;
+
 import java.lang.Math;
 import java.util.Scanner;
+
 public class Main {
-    public static class Task6
-    {
-        public static void main(String[] args){
+    public static class Task7 {
+        public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             System.out.print("Введите a =");
             double a = sc.nextDouble();
@@ -12,19 +13,19 @@ public class Main {
             double b = sc.nextDouble();
             System.out.print("Введите c =");
             double c = sc.nextDouble();
-            double d = b*b-4*a*c;
-            double x1 = (-b+Math.sqrt(d))/(2*a);
-            double x2 = (-b-Math.sqrt(d))/(2*a);
-            if (d<0) {
+            double d = b * b - 4 * a * c;
+            if (d < 0) {
                 System.out.println("Нет корней");
+            } else {
+                if (d == 0) {
+                    double x = (-b) / (2 * a);
+                    System.out.println("x = " + x);
+                } else {
+                    double x1 = (-b + Math.sqrt(d)) / (2 * a);
+                    double x2 = (-b - Math.sqrt(d)) / (2 * a);
+                    System.out.println("x1 = " + x1 + " x2 = " + x2);
+                }
             }
-            else if (x1==x2){
-                System.out.println("X = " + x1);
-            }
-            else{
-                System.out.println("X1 = " + x1 + " X2 = "+x2);
-            }
-
         }
     }
 }
